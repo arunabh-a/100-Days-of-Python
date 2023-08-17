e472749be3e4addb31f96062cc6c9a1fcea53cb0
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2023-08-17 17:43:53 +0530

    Day 8 Project Code Fix

--- Day 8/project/cipher.py
+++ Day 8/project/cipher.py
@@ -1,33 +1,39 @@
 # Caesar Cipher: Day 8 Project
-import ui
-
-# print(ui.logo)
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message: ").lower()
-shift = int(input("Type the shift number: "))
 
 def caesar(in_text, shift_amount, choice):
     result = ""
     listlen = len(ui.alphabet)
-    alphinlet = ui.alphabet.index(letter)
-    position = alphinlet + shift_amount
     for letter in in_text:
         if letter in ui.alphabet:
+            alphinlet = ui.alphabet.index(letter) #CMOL
+            position = alphinlet + shift_amount # CMOL
+        
             
             if choice == "encode":
                 position = alphinlet + shift_amount
-                # To avoid Index Overflow
-                if position > listlen - 1:
-                    position %= listlen
             elif choice == "decode":
                 position = alphinlet - shift_amount
-                # To avoid Index Overflow
-                if position > listlen - 1:
-                    position %= listlen
+            
+            # To avoid Index Overflow
+            if position > listlen - 1:
+                position %= listlen
             result += ui.alphabet[position]
         else:
-            result += ui.alphabet[position]
+            result += letter
 
     print(f"The {choice}d text: {result}\n\n")
-caesar(in_text = text, shift_amount = shift, choice =  direction)
+
+import ui
+
+print(ui.logo)
+while reinit == True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message: ").lower()
+    shift = int(input("Type the shift number: "))
+    caesar(in_text = text, shift_amount = shift, choice =  direction)
+
+    rest_req = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+
+    if rest_req == "no":
+        reinit = False
+        print("Utility Closed, Goodbye")
