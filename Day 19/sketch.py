431e19ff42dba4dc141e83ffe6911a13b65c2baa
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2023-09-01 00:03:50 +0530

    Day 19 Challenge Finish

---
+++ Day 19/sketch.py
@@ -0,0 +1,34 @@
+import turtle as t
+
+tim = t.Turtle()
+screen = t.Screen()
+
+
+
+def left():
+    lefthead = tim.heading() + 10
+    tim.setheading(lefthead)
+
+def right():
+    righthead = tim.heading() -  10
+    tim.setheading(righthead)
+
+def reverse():
+    tim.backward(10)
+
+def front():
+    tim.forward(10)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(key= "w", fun= front)
+screen.onkey(key= "a", fun= left)
+screen.onkey(key= "s", fun= reverse)
+screen.onkey(key= "d", fun= right)
+screen.onkey(key= "c", fun= clear)
+screen.exitonclick()
