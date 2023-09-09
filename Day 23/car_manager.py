7c4afa810e2c35c69a1c8547ed31d3495a0b7103
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2023-09-09 23:01:03 +0530

    Day 23 Continue Init

--- Day 23/car_manager.py
+++ Day 23/car_manager.py
@@ -1,7 +1,31 @@
+from turtle import Turtle
+import random as r
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self) :
+        self.carlist = []
+
+    def cargen(self):
+        chance = r.randint(1, 6)
+        if chance == 1:
+            car = Turtle()
+            car.shape("square")
+            car.shapesize(1,2)
+            car.color(r.choice(COLORS))
+            car.penup()
+            car.goto(310, r.randint(-250, 250))
+            self.carlist.append(car)
+    
+    def move_it(self):
+        for car in self.carlist:
+            car.setheading(180)
+            car.forward(STARTING_MOVE_DISTANCE)
+
+    def spedup(self):
+        STARTING_MOVE_DISTANCE += MOVE_INCREMENT
+        self.forward(STARTING_MOVE_DISTANCE)
+
--- Day 23/turtle_cross.py
+++ Day 23/turtle_cross.py
@@ -9,7 +9,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+play = Player()
+cars = CarManager()
+screen.listen()
+screen.onkey(play.move, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    for _ in range(2):
+        cars.move_it()
+    cars.cargen()
