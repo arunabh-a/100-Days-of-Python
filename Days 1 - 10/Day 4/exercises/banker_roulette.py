05a67bcaef7dd8451b1d4164b5d493824ebed87a
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2023-11-12 04:52:25 -0800

    Update banker_roulette.py

--- Days 1 - 10/Day 4/exercises/banker_roulette.py
+++ Days 1 - 10/Day 4/exercises/banker_roulette.py
@@ -5,5 +5,6 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
 randname = random.choice(names)
+# randname = r.randint (0, (len(names) - 1))
 
 print(randname + " is going to buy the meal today!")
